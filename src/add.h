ff68f2467156da7300111477e709a550e94ea2cb
matus <mat.fedorko@gmail.com>
2026-02-26 00:22:53 +0100

    Add common.h for the common layer code to bridge C++ and MSL

--- src/add.h
+++ src/add.h
@@ -1,15 +1,4 @@
-#if defined(__METAL_VERSION__)
-#define MTL_CONSTANT constant
-#define MTL_DEVICE device
-#define MTL_KERNEL kernel
-#else
-#define MTL_CONSTANT const
-#define MTL_DEVICE
-#define MTL_KERNEL static inline
-#include <cstdint>
-typedef uint32_t uint;
-#endif
-
+#include "common.h"
 
 MTL_KERNEL void add_kernel(
     uint index [[thread_position_in_grid]],
---
+++ src/common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#if defined(__METAL_VERSION__)
+#define MTL_CONSTANT constant
+#define MTL_DEVICE device
+#define MTL_KERNEL kernel
+#else
+#define MTL_CONSTANT const
+#define MTL_DEVICE
+#define MTL_KERNEL static inline
+#include <cstdint>
+typedef uint32_t uint;
+#endif
