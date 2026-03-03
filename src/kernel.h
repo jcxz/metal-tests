b977642481c27370a008c1ef1850da7747554ea1
matus <mat.fedorko@gmail.com>
2026-03-03 11:41:58 +0100

    WIP kernel runner

---
+++ src/kernel.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#if defined(__METAL_VERSION__)
+#define MTL_CONSTANT constant
+#define MTL_DEVICE device
+#define MTL_KERNEL kernel
+#include <metal_stdlib>
+#else
+#define MTL_CONSTANT const
+#define MTL_DEVICE
+#define MTL_KERNEL inline
+#include <simd/simd.h>
+#endif
+
+
+
