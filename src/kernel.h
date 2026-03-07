74b986ab7dc1be64072b61514dad4d8892c1ab61
matus <mat.fedorko@gmail.com>
2026-03-07 14:50:25 +0100

    Add [bufer(0)]] attribute to the metal version of DECL_KERNEL

--- src/kernel.h
+++ src/kernel.h
@@ -7,7 +7,7 @@
 #define MTL_CONSTANT constant
 #define MTL_DEVICE device
 #define DECL_KERNEL(ITYPE, NAME, ARGS) \
-	kernel void NAME(const ITYPE index [[thread_position_in_grid]], constant ARGS& args)
+	kernel void NAME(const ITYPE index [[thread_position_in_grid]], constant ARGS& args [[bufer(0)]])
 
 #define DECL_KERNEL_ARGS_BEGIN(NAME) struct NAME {
 // TODO make variadic
